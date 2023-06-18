35eeaf294244ffd11819a775b19834c02c5c7a8e
Gorobets Denis <orginux@protonmail.com>
2023-06-18 20:44:52 +0200

    fix pool

--- scripts/4/connections-pool.py
+++ scripts/4/connections-pool.py
@@ -1,27 +1,24 @@
 import psycopg2 as pg_driver
 import psycopg2.pool as pool
 
-pg_pool = pool.SimpleConnectionPool(1, 2, user="postgres", host="localhost")
+pg_pool = pool.SimpleConnectionPool(1, 4, user="postgres", host="localhost")
 # Инкапсулирует обращения к БД
 class Researcher:
     def __init__(self, id, university_id):
         self.id = id
         self.university_id = university_id
 
-    def getUniversity(self):
-        db = pg_pool.getconn()
-        try:
-            cur = db.cursor()
-            cur.execute('''
-                SELECT university_name FROM University WHERE university_id={0}
-                '''.format(self.university_id))
-            return cur.fetchone()[0]
-        finally:
-            pg_pool.putconn(db)
+    def getUniversity(self, db):
+        cur = db.cursor()
+        cur.execute('''
+        SELECT university_name FROM University WHERE university_id={0}
+        '''.format(self.university_id))
+        return cur.fetchone()[0]
 
 researchers = []
 ### Шаг 1: получаем из базы всех исследователей
-cur = pg_driver.connect(user="postgres", password="1212", host="localhost").cursor()
+db = pg_pool.getconn()
+cur = db.cursor()
 cur.execute("SELECT researcher_id, university_id FROM Researcher")
 
 ### Шаг 2: создаем объекты доступа к данным
@@ -30,6 +27,7 @@ for r in cur.fetchall():
 
 ### Шаг 3: печатаем университет, в котором работает каждый исследователь
 for r in researchers:
-    print(r.getUniversity())
+    print(r.getUniversity(db))
 
+pg_pool.closeall()
 cur.close()
